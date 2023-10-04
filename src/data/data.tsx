bacd467d9bb4ef4da8ba8e92929759cdac770b27
Abdelilah LAMHAMDI <65918024+ShadawRoot@users.noreply.github.com>
2023-10-04 23:35:24 +0200

    Update data.tsx
    Revert 3

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -81,12 +81,12 @@ export type SectionId = typeof SectionId[keyof typeof SectionId];
  */
 export const heroData: Hero = {
     imageSrc: heroImage,
-    name: `Hello, I am Abdelilah LAMHAMDI.`,
+    name: `I'm Abdelilah LAMHAMDI.`,
     description: (
         <>
             <p className="prose-sm text-stone-50 sm:prose-base lg:prose-lg">
-                I'm working as <strong className="text-sky-200">Infra & System Enginner </strong> & <i className="text-sky-200 fa-brands vmwarePic">CLOUD & ETHICAL HACKING </i> enthusiast. <br></br> Currently
-                working at  <strong className="text-sky-200">Confidential Company</strong> as a Full Stack Infra Enginner.
+                I'm working as <strong className="text-sky-500">Infra & System Enginner </strong> & <i className="text-sky-200 fa-brands vmwarePic">CLOUD & ETHICAL HACKING </i> enthusiast. <br></br> Currently
+                working at  <strong className="text-sky-200">CAPGEMINI</strong> as a Full Stack Infra Enginner.
             </p>
 
 
@@ -120,20 +120,16 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
     profileImageSrc: profilePic,
-    description: `In my opinion, information technology is not just about binary digits, but more about building strong and authentic values in companies.
-<<<<<<< HEAD
-Authenticity, respect, innovation, sharing, and commitment are strong values in my opinion to cultivate in a dynamic, innovative, simple, sometimes complex, but always surprising universe.`,
-=======
-Authenticity, respect, innovation, sharing, and commitment are strong values in my opinion to cultivate in a dynamic, innovative, simple, sometimes complex, but always surprising universe..
-I have a strong and diverse skill set in the field of information technology, particularly in areas related to digitalization, cloud computing, and information system security. 
-My expertise and proficiency in various technologies and domains make me well-equipped to provide valuable guidance and solutions to my customers.`,
->>>>>>> parent of 7c06b40 (Update data.tsx )
+    description: `I'm a diligent software developer with years of experience in e-commerce automatization's and overall
+  python based business applications.
+  Developer with none standard thinking, very often coming with creative new ideas or improvements... Not many know but I traveled half the world and visited more than 50 countries as of today. During my travels one country touched me the most, Japan.
+  I was amazed of the culture and the food and decided to take my enthusiasm a step forward. I studied about Japan and learned how to cook Japanese food. Mastered sushi making, so I am not just a software developer but secretly a chef too.`,
     aboutItems: [
-        {label: 'Location', text: 'Toulouse, France', Icon: MapIcon},
+        {label: 'Location', text: 'Center, Maroc', Icon: MapIcon},
         {label: 'Age', text: '40', Icon: CalendarIcon},
         {label: 'Nationality', text: 'Marocaine', Icon: FlagIcon},
-        {label: 'Interests', text: 'IT,Cloud, Security, DevOps, Design, Diving', Icon: SparklesIcon},
-        {label: 'Study', text: 'Pascal Poali, France', Icon: AcademicCapIcon},
+        {label: 'Interests', text: 'IT, Gadgets, Diving', Icon: SparklesIcon},
+        {label: 'Study', text: 'HackerU Collage, Maroc', Icon: AcademicCapIcon},
         {label: 'Employment', text: 'Freelancer', Icon: OfficeBuildingIcon},
     ],
 };
@@ -322,36 +318,10 @@ export const FavoriteTechItems: FavoriteTechItem[] = [
  */
 export const education: TimelineItem[] = [
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        date: '2010-2011',
-        location: 'FENA Business School ',
-        title: 'Mangment',
-        content: <p>Project and portfolio management (PPM)</p>,
-        <p>Realization of IT dashboards and KPI</p>,
-=======
-=======
->>>>>>> parent of 57b1412 (Update data.tsx)
-=======
->>>>>>> parent of 57b1412 (Update data.tsx)
-=======
->>>>>>> parent of 57b1412 (Update data.tsx)
         date: '2022-2023',
         location: 'HackerU Collage',
         title: 'Java, OCA',
         content: <p>Java Full-Stack studies with mobile applications development orientation and modern frameworks.</p>,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 57b1412 (Update data.tsx)
-=======
->>>>>>> parent of 57b1412 (Update data.tsx)
-=======
->>>>>>> parent of 57b1412 (Update data.tsx)
-=======
->>>>>>> parent of 57b1412 (Update data.tsx)
     },
     {
         date: '2020-2020',
