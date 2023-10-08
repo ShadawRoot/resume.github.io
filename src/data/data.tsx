35b57b888ad8568bcc778650c618f7ae9f760de7
Abdelilah LAMHAMDI <65918024+ShadawRoot@users.noreply.github.com>
2023-10-08 18:24:41 +0200

    Update data.tsx

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -102,7 +102,7 @@ export const heroData: Hero = {
     ),
     actions: [
         {
-            href: 'https://drive.google.com/file/d/1M5jndPJIw2H-3_0ZJl39I8PsDZCZmLI-/view?usp=drive_link',
+            href: '/src/images/CV_abdelilah_lamhamdi.pdf',
             text: 'Resume',
             primary: true,
             Icon: DownloadIcon,
